d92eddc7e2d3611f879542225602155dc1fe3c81
Héctor <hartisca91@gmail.com>
2025-02-06 15:03:17 +0100

    First push

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = FastAPI()
+API_KEY = os.getenv("API_KEY")
+
+@app.get("/weather/")
+def get_weather(city: str):
+    if not API_KEY:
+        return {"error": "API Key no configurada"}
+
+    url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{city}?unitGroup=metric&key={API_KEY}&contentType=json"
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        return {"error": "No se pudo obtener el clima", "status_code": response.status_code}
+
+    return response.json()
