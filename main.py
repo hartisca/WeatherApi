0ede4b86b0015ae8ee50b78aa61832a96c65749d
Héctor <hartisca91@gmail.com>
2025-02-12 20:31:06 +0100

    Cors added and working

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import requests
 import os
 from dotenv import load_dotenv
@@ -6,6 +7,18 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = FastAPI()
+origins = [
+    "http://localhost",
+    "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 API_KEY = os.getenv("API_KEY")
 
 @app.get("/weather/")
